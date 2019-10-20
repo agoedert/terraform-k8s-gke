1efca0b484b9074eef182d4dcc1907f3b108498a
Alexandre Goedert <alexandre.goedert@thoughtworks.com>
2019-10-20 20:48:53 -0300

    @agoedert: creating syntetic load and testing with locust

--- app/index.js
+++ app/index.js
@@ -2,10 +2,19 @@ var express = require('express');
 var app = express();
 var port = 8081;
 
+const syntheticLoad = () => {
+    var counter = 0;
+    for (var i = 0; i < 100000000; i++) {
+        counter++;
+    }
+    console.log(`iterated ${counter} times`);
+};
+
 app.get('/', function (req, res) {
-   res.send('Hello World');
+    syntheticLoad(); 
+    res.send('Hello World');
 })
 
-var server = app.listen(port, function () {
-   console.log(`Example app listening at port ${port}`)
+app.listen(port, function () {
+    console.log(`Example app listening at port ${port}`)
 })
